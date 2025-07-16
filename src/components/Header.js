a1cee276e500b7a4e9af27404200b5c8daa221f2
Patrick Pei <ppei+575@coursera.org>
2025-07-16 09:02:37 -0700

    add

--- src/components/Header.js
+++ src/components/Header.js
@@ -14,7 +14,7 @@ const Header = ({ darkMode, toggleTheme }) => {
         <p>I'm <strong>Patrick Pei</strong>, a <strong>senior software engineer</strong> with a passion for building <strong>distributed systems</strong> & creating impactful solutions</p>
       </div>
       <div className="contact">
-        <h3>Drop me a line <a href="mailto:yingjianpei2022@u.northwestern.edu">yingjianpei2022@u.northwestern.edu</a></h3>
+        <h3>Drop me a line <a href="mailto:yingjianpei2022@u.northwestern.edu">yingjianpei2022@u.northwestern.edu!</a></h3>
       </div>
     </header>
   );
